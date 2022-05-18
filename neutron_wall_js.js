a70db5dd22a4f5bce28645c717715aeafdae2f57
Fanurs Teh <fyanurs@gmail.com>
2022-05-18 16:25:20 -0400

    :bug: Fixed OrbitControls

--- neutron_wall_js.js
+++ neutron_wall_js.js
@@ -1,5 +1,5 @@
-// import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js";
-// import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -9,7 +9,7 @@ document.body.appendChild( renderer.domElement );
 
 const scene = new THREE.Scene();
 
-// const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
 
 const board = new THREE.Group();
 
@@ -22,37 +22,26 @@ for(let i = 0; i < 24; i++)
     let material;
     if((i+1) % 2 == 1)
     {
-    	material = new THREE.MeshBasicMaterial( { color: odd_color } );
+        material = new THREE.MeshBasicMaterial( { color: odd_color } );
     }
     else
     {
-    	material = new THREE.MeshBasicMaterial( { color: even_color } );
+        material = new THREE.MeshBasicMaterial( { color: even_color } );
     }
     const cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
     cube.position.y = i * 8.255;
     board.add(cube);
 }
 
 scene.add(board);
 
-// const geometry = new THREE.BoxGeometry(193.675,8.255,6.985);
-// const material = new THREE.MeshBasicMaterial( { color: odd_color } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 camera.position.set(200,200,200);
 camera.lookAt(0,0,0);
-// controls.update();
-
-
+controls.update();
 
 function animate() {
     requestAnimationFrame( animate );
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    // controls.update();
-
     renderer.render( scene, camera );
 };
 
