b07853c3691a553e3b451129b75a01884de02b7e
poulomidey <93102914+poulomidey@users.noreply.github.com>
2022-06-02 09:23:32 -0400

    Adding extras (#13)
    * fixed x and z spacing
    
    * updated dist bw n wall and v wall
    
    * updated code to fix dist bw n and v wall
    
    * separated functions into different files
    
    * added microball, but still errors in dimensions
    
    * changed microball function call

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
+import {create_neutron_wall} from "./neutron_wall.js";
+import {create_veto_wall} from "./veto_wall.js";
+import {create_microball} from "./microball.js";
 
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -8,99 +11,37 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color( 0xffffff );
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// const v_wall_z_dist = 1.0008;
+// const dist_bw_neutron_veto = 61.085; //DOUBLE CHECK DIST BW N WALL AND V WALL. IS N WALL TILTED?
+
 const neutron_wall = new THREE.Group();
-
-function create_neutron_wall(scene, neutron_wall, bar_num, x_dist, y_dist, z_dist, spacing, dist_bw_neutron_veto)
-{
-    for (let i = 0; i < bar_num; i++) {
-        const geometry = new THREE.BoxGeometry(x_dist, y_dist, z_dist);
-        let materialProperties;
-        if (i % 2 == 0) {
-            materialProperties = {
-                color: "hsl(228, 100%, 78%)",
-            };
-        } else {
-            materialProperties = {
-                color: "hsl(228, 100%, 50%)",
-            };
-        }
-        let material = new THREE.MeshBasicMaterial(materialProperties);
-        const bar = new THREE.Mesh(geometry, material);
-        bar.position.x += x_dist/2;
-    
-        if(i != 0)
-        {
-            bar.position.y += i * (y_dist + spacing);
-        }
-        
-        neutron_wall.add(bar);
-    }
-    
-    scene.add(neutron_wall);
-    neutron_wall.position.z -= dist_bw_neutron_veto;
-}
-
-create_neutron_wall(scene, neutron_wall, 24, 193.675, 8.255, 6.985, .3175, 100); //DIST BW N-WALL AND V-WALL NOT CORRECT
+create_neutron_wall(scene, neutron_wall, 24, 193.675, 8.255, 6.985, .3175);
+// neutron_wall.rotateY(3.14/4);
 
 //veto wall
 const veto_wall = new THREE.Group();
+create_veto_wall(scene, veto_wall, 23, 9.398, 227.2362, 1.0008, .3, .15);
 
-function create_veto_wall(scene, veto_wall, bar_num, x_dist, y_dist, z_dist, x_spacing, z_spacing)
-{
-    for(let i = 1; i <= 23; i++)
-    {
-        let zigzag = z_spacing;
-        const geometry = new THREE.BoxGeometry(x_dist, y_dist, z_dist);
-        let materialProperties;
-        if (i % 2 != 0) {
-            materialProperties = {
-                color: "hsl(320, 0%, 79%)",
-            };
-            zigzag *= -1;
-        } else {
-            materialProperties = {
-                color: "hsl(320, 0%, 49%)",
-            };
-        }
-        let material = new THREE.MeshBasicMaterial(materialProperties);
-        const bar = new THREE.Mesh(geometry, material);
-        bar.position.y += y_dist/2;
-        bar.position.z += zigzag;
-        if(i != 0)
-        {
-            bar.position.x += i * (x_dist + x_spacing);
-        }
-        
-        veto_wall.add(bar);
-    
-    }
-    veto_wall.position.x -= x_dist/2;
-    scene.add(veto_wall);
-}
+// const axesHelper = new THREE.AxesHelper( 600 );
+// scene.add( axesHelper );
 
-create_veto_wall(scene, veto_wall, 23, 9.398, 227.2362, 1.0008, .5, .5); //X_SPACING AND Z_SPACING NOT CORRECT
+create_microball(scene); //make microball group in future and add rings to microball
 
-// let bar_num = 23;
-// let x_dist = 9.398;
-// let y_dist = 227.2362;
-// let z_dist = 1.0008;
-// let x_spacing = .5; //NOT CORRECT
-// let z_spacing = .5; //NOT CORRECT
+neutron_wall.position.z -= 200;
+veto_wall.position.z -= 300;
 
 
 
-// const geometry = new THREE.BoxGeometry(x_dist, y_dist, z_dist);
-// let materialProperties = {color: "hsl(320, 0%, 79%)",};
-// let material = new THREE.MeshBasicMaterial(materialProperties);
-// const bar = new THREE.Mesh(geometry, material);
-// bar.position.y += y_dist/2;
-// scene.add(bar);
 
-const axesHelper = new THREE.AxesHelper( 600 );
-scene.add( axesHelper );
+const size = 600;
+const divisions = 50;
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
 
 camera.position.set(200, 200, 200);
 camera.lookAt(0, 0, 0);
---
+++ src/neutron_wall.js
@@ -0,0 +1,32 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
+
+export function create_neutron_wall(scene, neutron_wall, bar_num, x_dist, y_dist, z_dist, spacing)
+{
+    for (let i = 0; i < bar_num; i++) {
+        const geometry = new THREE.BoxGeometry(x_dist, y_dist, z_dist);
+        let materialProperties;
+        if (i % 2 == 0) {
+            materialProperties = {
+                color: "hsl(228, 100%, 78%)",
+            };
+        } else {
+            materialProperties = {
+                color: "hsl(228, 100%, 50%)",
+            };
+        }
+        let material = new THREE.MeshBasicMaterial(materialProperties);
+        const bar = new THREE.Mesh(geometry, material);
+        bar.position.x += x_dist/2;
+    
+        if(i != 0)
+        {
+            bar.position.y += i * (y_dist + spacing);
+        }
+        
+        neutron_wall.add(bar);
+    }
+    
+    scene.add(neutron_wall);
+    // neutron_wall.position.z -= dist_bw_neutron_veto + (2*v_wall_z_dist + .3)/2;
+    neutron_wall.position.y += y_dist/2;
+}
